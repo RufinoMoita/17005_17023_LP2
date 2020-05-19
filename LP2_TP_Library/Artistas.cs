1a43ba9f3bdb37d3ffee6efd955b163243ba363f
Hélder Sá <a17023@alunos.ipca.pt>
2020-05-19 18:57:34 +0100

    Developing in layers - Part I

--- LP2_TP_Library/Artistas.cs
+++ LP2_TP_Library/Artistas.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LP2_TP_Library
+﻿namespace LP2_TP_Library
 {
     class Artistas
     {
